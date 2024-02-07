e9046c5b31b57eaae327714c20c7a99aa5337d73
김수빈 <ambitiousksb@gmail.com>
2024-02-07 13:41:02 +0900

    rbtree

--- src/rbtree.h
+++ src/rbtree.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 
+/* 1️⃣ RB tree 구조체 생성 */
 typedef enum { RBTREE_RED, RBTREE_BLACK } color_t;
 
 typedef int key_t;
@@ -13,20 +14,29 @@ typedef struct node_t {
   struct node_t *parent, *left, *right;
 } node_t;
 
-typedef struct {
+typedef struct rbtree {
   node_t *root;
-  node_t *nil;  // for sentinel
+  node_t *nil;
 } rbtree;
 
 rbtree *new_rbtree(void);
 void delete_rbtree(rbtree *);
+void traverse_and_delete_node(rbtree *, node_t *);
 
 node_t *rbtree_insert(rbtree *, const key_t);
+void rbtree_insert_fixup(rbtree *, node_t *);
+void right_rotate(rbtree *, node_t *);
+void left_rotate(rbtree *, node_t *);
+void exchange_color(node_t *, node_t *);
+
 node_t *rbtree_find(const rbtree *, const key_t);
 node_t *rbtree_min(const rbtree *);
 node_t *rbtree_max(const rbtree *);
 int rbtree_erase(rbtree *, node_t *);
 
 int rbtree_to_array(const rbtree *, key_t *, const size_t);
+
+node_t *get_next_node(const rbtree *, node_t *);
+void rbtree_erase_fixup(rbtree *, node_t *, int);
 
 #endif  // _RBTREE_H_
